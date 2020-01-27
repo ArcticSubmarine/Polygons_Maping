d91c887482eeaa69c2c181a4c4a5271b908e2a9b
Clarisse <48919999+ArcticSubmarine@users.noreply.github.com>
2020-01-27 19:28:49 +0100

    Add files via upload

---
+++ Polygons_Maping/routes/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+const bodyParser = require('body-parser');
+
+/* GET home page. */
+router.use(bodyParser.json());
+router.use(
+    bodyParser.urlencoded({extended: true,})
+);
+
+router.get('/map', function (request, response) {
+    response.sendFile('map.html', {root: 'Polygons_Mapping/views'});
+});
+
+module.exports = router;
